2b1ae572d0e5fbf9bce77e0f2d5e2e2faea3558b
Jehuti-ms <dmoseley@gams.edu.bb>
2025-11-04 14:51:17 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Supabase Configuration
 const SUPABASE_CONFIG = {
-    url: 'https://your-project-ref.supabase.co',
+    url: 'https://wjzkiceausyejnmnlqvg.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqemtpY2VhdXN5ZWpubW5scXZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMzg3ODEsImV4cCI6MjA3NzgxNDc4MX0.uqX0SA5Wa52yPvRkSxxrkDvC8YkIEpO5MNndAC_IrHQ'
 };
 
