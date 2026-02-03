3faed5b09295e57a42b2be42c7867a9f88af4ca6
Jehuti-ms <dmoseley@gams.edu.bb>
2026-02-02 23:10:50 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
-// Firebase configuration
+// Firebase configuration - VERIFY THIS MATCHES YOUR FIREBASE PROJECT
 const firebaseConfig = {
-  apiKey: "AIzaSyAagSPJW2RxyG28Og54ftYd8MGvPPKO_SE",
-  authDomain: "broilerprojectclaimform-d6d51.firebaseapp.com",
-  projectId: "broilerprojectclaimform-d6d51",
-  storageBucket: "broilerprojectclaimform-d6d51.firebasestorage.app",
-  messagingSenderId: "1069004689384",
-  appId: "1:1069004689384:web:ab7e2e4063e2ee864c5e5d",
-  measurementId: "G-ST930670JC"
+    apiKey: "AIzaSyAagSPJW2RxyG28Og54ftYd8MGvPPKO_SE",
+    authDomain: "broilerprojectclaimform-d6d51.firebaseapp.com",
+    projectId: "broilerprojectclaimform-d6d51",
+    storageBucket: "broilerprojectclaimform-d6d51.firebasestorage.app",
+    messagingSenderId: "1069004689384",
+    appId: "1:1069004689384:web:ab7e2e4063e2ee864c5e5d",
+    measurementId: "G-ST930670JC"
 };
 
 // Initialize Firebase
